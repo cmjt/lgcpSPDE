01e35367162c3f098df7f4790316949126d20349
Charlotte <cmjt@st-andrews.ac.uk>
2016-05-17 13:51:30 +0100

    initial

---
+++ R/make_mesh.r
@@ -0,0 +1,45 @@
+#' Wrapper function to create a mesh object as used in an INLA model fit
+#'
+#'
+#' @return A ``mesh'' object used in an INLA/SPDE model
+#'
+#' @param locs a matrix of locations, either this or \code{spatial.polygon} must be supplied.
+#' @param mesh.pars a named vertor of mesh parameters, must contain
+#' \code{cutoff} length at which to cut off triangle edge lengths,
+#' \code{max.edge.min} triangle edge length inside region,
+#' and \code{max.edge.max} triangle edge length inside region.
+#' @param spatial.polygon if supplied the spatial polygon for the domain is used to construct mesh
+#' @param sphere Logical if TRUE the mesh is constructed on the unit sphere, note this is only possible if coordinates are
+#' longitude and Latitude, by default FALSE
+#' 
+#' @export
+make.mesh<-function(locs = NULL, mesh.pars = NULL, spatial.polygon = NULL, sphere = FALSE){
+    # getting mesh parameters
+    max.edge.min <- mesh.pars["max.edge.min"]
+    max.edge.max <- mesh.pars["max.edge.max"]
+    max.edge <- c(max.edge.min,max.edge.max)
+    cutoff <- mesh.pars["cutoff"]
+    mesh.pars<-c(max.edge,cutoff)
+    if(!sphere){
+        if(!is.null(spatial.polygon)){
+            # creates triangulation based on a spatial polygon of the domain
+            boundary <- inla.sp2segment(spatial.polygon)
+            mesh <- inla.mesh.2d(boundary = boundary, max.edge = max.edge, cutoff = cutoff)
+        } else {
+            loc <- locs
+            # creates triangulation based on the locations of the point pattern
+            mesh <- inla.mesh.2d(loc = locs, max.edge = max.edge, cutoff = cutoff)
+        }}
+    if(sphere){
+        if(!is.null(spatial.polygon)){
+            # creates triangulation based on a spatial polygon of the domain projected onto a sphere
+            boundary <- inla.sp2segment(spatial.polygon)
+            boundary$loc <- inla.mesh.map(boundary$loc, projection="longlat", inverse=TRUE)
+            mesh <- inla.mesh.2d(boundary = boundary, max.edge = max.edge, cutoff = cutoff)
+        } else {
+            # creates triangulation based on the locations of the point pattern projected onto a sphere
+            locs <- inla.mesh.map(locs, projection="longlat", inverse=TRUE)
+            mesh <- inla.mesh.2d(loc = locs, max.edge = max.edge, cutoff = cutoff)
+        }}
+    mesh
+}
---
+++ R/rgeospde.r
@@ -0,0 +1,38 @@
+#' Function to simulate from a spatial/spatio temporal SPDE model
+#'
+#'
+#' @return A matrix of values each column a set of observations at each time point
+#'
+#' @param n a numeric constant defining the number of time points, by default 1.
+#' @param locs a matrix of locations at which the values are to be simulated
+#' @param mesh a ``mesh'' object i.e. delauney triangulation of the domain, an
+#' object returned by \link{make.mesh}.
+#' @param kappa a numeric constant, parameter of the SPDE model.
+#' @param sigma2 a numeric constant, parameter of the SPDE model, by default this is 1.
+#' @param rho the ar1 correlation coefficient for spatio-temporal samples,
+#' by default this is 0.9.
+#' @param seed seed for the simulation, by default this is 1
+#' @export
+rgeospde <- function( locs = NULL,mesh = NULL, kappa = NULL, sigma2 = 1,n = 1, rho = 0.9, seed = 1){
+    # define the spde model for the domain
+    spde <- inla.spde2.matern(mesh = mesh, alpha=2)
+    # define theta
+    theta <- c(-0.5 * log(4 * pi * sigma2 * kappa^2), log(kappa))
+    # precision matrix
+    Q <- inla.spde2.precision(spde, theta)
+    #projector matrix
+    A <- inla.mesh.project(mesh = mesh,loc = locs)$A
+    if (n == 1) {
+        # sample at the mesh nodes
+        x <- inla.qsample(Q = Q, seed = seed, constr = spde$f$extraconstr)
+        x <- drop(A%*%x)
+    } else {
+        x.t <- inla.qsample (n = n, Q = Q, seed = seed, constr = spde$f$extraconstr)
+        x.t <- drop(A%*%x.t)
+        x <- x.t
+        for (j in 2:n){
+            x[,j] <- rho*x[,j-1] + sqrt(1-rho^2)*x.t[,j]
+        }
+    }
+    x
+}
