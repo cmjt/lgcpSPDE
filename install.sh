5e773339d1ed2d3dd7cbf1e93279f0a38d65a353
cmjt <cmjt@st-andrews.ac.uk>
2016-08-11 12:22:04 +0100

    install

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd ~/Documants/Gitwork/lgcpSPDE
+rm -rfv man
+rm -fv NAMESPACE
+R --slave -e "library(Rcpp); compileAttributes()"
+R --slave -e "library(roxygen2); roxygenise('.')"
+rm -rfv ..Rcheck/ ..pdf
+rm -rfv src/*.o src/*.so src/*.rds
+rm -rfv src-i386/ src-x64/
+R CMD build .
+mkdir -p package-build
+mv lgcpSPDE_*.tar.gz package-build/
+R CMD check package-build/lgcpSPDE_*.tar.gz
+R CMD INSTALL package-build/lgcpSPDE_*.tar.gz
