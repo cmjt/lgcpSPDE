7a1565f230442914ec9e3ec2ffa8caaae037f8c8
Charlotte <cmjt@st-andrews.ac.uk>
2016-09-16 16:23:13 +0100

    initial commit for random kappa

--- src/nskappaspde.cpp
+++ src/nskappaspde.cpp
@@ -11,21 +11,23 @@ Type objective_function<Type>::operator() ()
   DATA_IVECTOR(meshidxloc);
   DATA_STRUCT(spde,spde_t); // this as structure of spde object is defined in r-inla hence using that namespce
   PARAMETER(beta0); // intercept term
-  //PARAMETER(log_tau); 
-  PARAMETER(log_kappa); // kappa of random field
+  PARAMETER(log_kappa); // fixed kappa of random field
+  PARAMETER_VECTOR(ns_log_kappa);
+  PARAMETER(sigma_u); //variance on u (random effect on kappa
+  vector<Type> u(points.size()); //the random bit of ns_log_kappa
   PARAMETER_VECTOR(x); //the random field/effect
-  //Type tau = exp(log_tau); 
-  Type kappa = exp(log_kappa); // return the kappa parameter of the Random field
+  Type kappa = exp(log_kappa); // return the (fixed) kappa parameter of the Random field
+  Type sum_u = 0;
   SparseMatrix<Type> Q = Q_spde(spde,kappa); // create the precision matrix from the spde model for the GMRF
   Type nll = GMRF(Q)(x); // x the random effect is a GMRF with precision Q
   for(int i = 0; i <points.size(); i++){
+    sum_u += u(i);
+    ns_log_kappa(i) = log_kappa + sum_u;
     Type eta = beta0 + log(area(i)) + x(i); 
     Type lambda = exp(eta); // intensity 
-    nll -= dpois(points(i),lambda,true); 
+    nll -= dpois(points(i),lambda,true); // contribution from observed data
+    nll -=dnorm(u(i),Type(0),sigma_u,true);
   }
-  double nu = 1.0;            // nu = alpha-d/2 = 2-1 by eqn (2) in Lindgren 
-  Type rho = sqrt(8*nu)/kappa;  // Distance at which correlation has dropped to 0.1 (p.  4 in Lindgren)
-  ADREPORT(rho);
   ADREPORT(kappa);
   return nll;
 
