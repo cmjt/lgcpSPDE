4b54ea8076f184fb7e38a3870016cb06e910689c
Charlotte <cmjt@st-andrews.ac.uk>
2016-09-30 16:31:28 +0100

    bug fix

--- inst/tmb/src/nonstatspde.hpp
+++ inst/tmb/src/nonstatspde.hpp
@@ -26,16 +26,17 @@ namespace non_stat {
   SparseMatrix<Type> Q_spde_nonstat(spde_nonstat_t<Type> spde, Type theta2, Type theta3){
     vector<Type> log_phi1;
     vector<Type> log_phi2;
+    // the indecies should be 0,2,and 3 we are ignoring tau. Note ensure B1,B2 are nx4 matrices
     log_phi1 = spde.B1.col(0) +  spde.B1.col(2)*theta2 + spde.B1.col(3)*theta3;
     log_phi2 = spde.B2.col(0) +  spde.B2.col(2)*theta2 + spde.B2.col(3)*theta3;
     SparseMatrix<Type> D1(log_phi1.size(),log_phi1.size());
     SparseMatrix<Type> D2(log_phi1.size(),log_phi1.size());
     for(int i=0; i <log_phi1.size(); i++){
       for (int j=0; j<log_phi1.size(); j++){
-    	if(i==j){ 
-    	  D1.coeffRef(i,j) = exp(log_phi1(i));
-    	  D2.coeffRef(i,j) = exp(log_phi2(i));
-    	}}}
+	if(i==j){ 
+	  D1.coeffRef(i,j) = exp(log_phi1(i));
+	  D2.coeffRef(i,j) = exp(log_phi2(i));
+	}}}
     return D1*spde.M0*D1 + D2*D1*spde.M1 + spde.M1.transpose()*D1*D2 + spde.M2;
   }
 } // end namespace non_stat
