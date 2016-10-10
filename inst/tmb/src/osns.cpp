3386d9d35f5c8fff3e318a1c9be935da471c510f
cmjt <cmjt@st-andrews.ac.uk>
2016-10-10 13:36:01 +0100

    log_kappa

--- inst/tmb/src/osns.cpp
+++ inst/tmb/src/osns.cpp
@@ -13,10 +13,11 @@ Type objective_function<Type>::operator() ()
   DATA_VECTOR(area);
   DATA_IVECTOR(meshidxloc);
   DATA_STRUCT(spde,spde_oscilate_t); // this as structure of spde object is defined in r-inla hance using that namespce
-  PARAMETER(kappa);
+  PARAMETER(log_kappa);
   PARAMETER(theta); 
   PARAMETER(beta0); // intercept term
   PARAMETER_VECTOR(x); //the random field/effect
+  Type kappa = exp(log_kappa);
   SparseMatrix<Type> Q = Q_spde_oscilate(spde,kappa,theta); // create the prec matrix from the spde model for the GMRF
   Type nll = GMRF(Q)(x); // x the random effect is a GMRF with precision Q
   for(int i = 0; i <resp.size(); i++){
@@ -26,5 +27,6 @@ Type objective_function<Type>::operator() ()
   }
   Type inlogittheta  = exp(theta)/(1+exp(theta)); 
   ADREPORT(inlogittheta);
+  ADREPORT(kappa);
   return nll;
 }
