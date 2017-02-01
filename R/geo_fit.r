37088d77d3c7489800a60fbaf3aaea8460cb07a5
cmjt <cmjt@st-andrews.ac.uk>
2017-02-01 16:35:57 +1300

    listing required arguments

--- R/geo_fit.r
+++ R/geo_fit.r
@@ -8,10 +8,24 @@
 #' @param locs a matrix of observation locations, where each row corresponds to the observation. 
 #' @param response a vector of response variable, each corresponds to the spatial locations
 #' in \code{locs}.
-#' @param temp a numeric vector specifying a temporal index for each observation (starting at 1.....T).
-#' @param covariates a named data.frame of covariates 
+#' @param temp (optional) a numeric vector specifying a temporal index for each observation (starting at 1.....T).
+#' @param covariates (optional) a named data.frame of covariates.
 #' @param family a character vector specifying the assumed likelihood of the response, by default is "gaussian".
-#' @param prior.rho prior for the temporal correlation coefficient, by default a \code{pcprior} is used with \code{param=c(0-0.9)}. 
+#' @param control.time (optional) supplied if the \code{temp} argumet is given to fit a spatio-temporal model. This argument
+#' controls the model and prior put on the hyperparameters of the model for the temporal component of the spatio-temporal
+#' model. By default this is \code{list(model = 'ar1', param = list(theta = list(prior='pccor1', param = c(0, 0.9))))}
+#' which is a pc.prior put on the rho coefficient of a AR(1) model with P(rho>0)=0.9.
+#' Refer to Simpson, martins, and rue for further details *****put in proper refs*****
+#' @param control.inla a list which controls the fitting procedures INLA uses see Rue et al. ***ref book***
+#' by default this is \code{list(strategy='gaussian',int.strategy = 'eb')} for quick and dirty fitting.
+#' @param control.compute a list of fit statistics the user wants INLA to return. By default this
+#' is \code{list(dic = TRUE, waic = TRUE,cpo = TRUE, config = TRUE)}.
+#' @param non.linear (optional) should be used if the user requires a non-linear covariate to be included in the model
+#' Must be supplied as a named list with elements \code{random.effect} a numeric vector of the random effect indecies,
+#' and \code{model} the random effect model the user wishes to use for \code{random.effect}
+#' @param prediction (optional) should be used if the uses wnts to run a prediction model. Must be supplied as a
+#' named list with \code{pred.locs} the locations where predictionas are to be made, and only if a spatio-tempral
+#' model is to be fitted \code{pred.temp} the temporal indecies for the predictions (the same length as \code{pred.locs}).
 #' @param verbose Logical if \code{TRUE} model fit is output to screen.
 #'
 #'
@@ -25,7 +39,7 @@
 
 geo.fit <- function(mesh = NULL,  locs = NULL, response = NULL, temp = NULL,covariates = NULL, family = "gaussian",
                     control.time = list(model = 'ar1', param = list(theta = list(prior='pccor1', param = c(0, 0.9)))),
-                    control.inla=list(strategy='gaussian',int.strategy = 'eb'),
+                    control.inla = list(strategy='gaussian',int.strategy = 'eb'),
                     control.compute = list(dic = TRUE, waic = TRUE,cpo = TRUE, config = TRUE),
                     non.linear = NULL, prediction = NULL, verbose = FALSE){
     if(is.null(temp)&is.null(covariates)){
@@ -304,72 +318,6 @@ geo.spatial.temporalfit <- function(mesh, locs, response,temp, covariates, temp,
                    verbose = verbose)
     return(result)
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-geo.st.fit <- function(mesh = NULL,  locs = NULL, response = NULL, temp = NULL,covariates = NULL, family = "gaussian",
-                       ctr.g = list(model = 'ar1', param = list(theta = list(prior='pccor1', param = c(0, 0.9)))),
-                       control.inla=list(strategy='gaussian',int.strategy = 'eb'),
-                       control.compute = list(dic = TRUE, waic = TRUE,cpo = TRUE, config = TRUE),
-                       non-linear = NULL, prediction = NULL, verbose = FALSE){
-                                        # number of mesh nodes
-    nv <- mesh$n
-    ## number of years and temporal mesh
-    k <- (mesh.t <- inla.mesh.1d(temp))$n
-    # number of observations
-    n <- nrow(locs)
-    # the spde model for the latent field
-    spde <- inla.spde2.matern(mesh = mesh, alpha = 2)
-    # the indecies for the latent spatio-temporal field
-    field <- inla.spde.make.index('field',n.spde = spde$n.spde, group = temp, n.group = k)
-    # the projection matrix
-    Ast <- inla.spde.make.A(mesh = mesh, loc = locs ,group = temp, n.group = k)
-    # define variables
-    Y <- response
-    ## covariates
-    n.covs <- ncol(covariates)
-    for(i in 1:n.covs){
-        assign(colnames(covariates)[i],covariates[,i],envir = .GlobalEnv)
-    }
-    effects = list(field = field, sapply(colnames(covariates),get,simplify = FALSE),
-                                   b0 = rep(1,length = n))
-    # temporal model "ar1"
-    ctr.g <- list(model='ar1',param = prior.rho)
-    # model formula
-    formula = paste("y", "~  0 + b0 +", paste(colnames(covariates),collapse = " + "),
-                    " + f(field, model = spde, group = field.group, control.group = ctr.g)")
-    stack <- inla.stack(data = list(y = Y),
-                            A = list(Ast,1, 1),
-                            tag = "response",
-                            effects = effects)
-    result <- inla(as.formula(formula),
-                   family = family,
-                   data=inla.stack.data(stack),
-                   control.predictor=list(A=inla.stack.A(stack)),
-                   control.compute = list(config = TRUE),
-                   control.inla  = control.inla,
-                   verbose = verbose)
-    attributes <- list()
-    attributes$stack <- stack
-    attributes(result) <- c(attributes(result), attributes)
-    result
-
-}
-
-
 
 
 
