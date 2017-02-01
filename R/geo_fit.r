21729d8b4359c8a7bafb22ab60e491d9804120c1
cmjt <cmjt@st-andrews.ac.uk>
2017-02-01 16:40:58 +1300

    typos

--- R/geo_fit.r
+++ R/geo_fit.r
@@ -180,7 +180,7 @@ geo.spatial.fit <- function(mesh, locs, response, covariates, family, control.in
     }
     result <- inla(as.formula(formula), family = family,
                    data = inla.stack.data(stack),
-                   A=inla.stack.A(stack),
+                   control.predictor=list(A=inla.stack.A(stack)),
                    control.inla = control.inla,
                    control.compute = control.compute,
                    verbose = verbose)
@@ -312,7 +312,7 @@ geo.spatial.temporal.fit <- function(mesh, locs, response, covariates, temp,  fa
     }
     result <- inla(as.formula(formula), family = family,
                    data = inla.stack.data(stack),
-                   A=inla.stack.A(stack),
+                   control.predictor=list(A=inla.stack.A(stack)),
                    control.inla = control.inla,
                    control.compute = control.compute,
                    verbose = verbose)
