e23ff190bc28a99da3a486795f29f08b617d30bc
cmjt <cmjt@st-andrews.ac.uk>
2017-02-20 10:13:39 +1300

    joint fit as per python

--- R/geo_joint_fit.r
+++ R/geo_joint_fit.r
@@ -17,84 +17,90 @@
 #' Refer to Simpson, martins, and rue for further details *****put in proper refs*****
 #' @param control.inla a list which controls the fitting procedures INLA uses see Rue et al. ***ref book***
 #' by default this is \code{list(strategy='gaussian',int.strategy = 'eb')} for quick and dirty fitting.
+#' @param hyper prior for the copy parameter by default is a N(0,10) i.e.,  list(theta=list(prior='normal', param=c(0,10)))
 #' @param control.compute a list of fit statistics the user wants INLA to return. By default this
 #' is \code{list(dic = TRUE, waic = TRUE,cpo = TRUE, config = TRUE)}.
 #' @param non.linear (optional) should be used if the user requires a non-linear covariate to be included in the model
 #' Must be supplied as a named list with elements \code{random.effect} a numeric vector of the random effect indecies,
 #' and \code{model} the random effect model the user wishes to use for \code{random.effect}
-#' @param prediction (optional) should be used if the uses wnts to run a prediction model. Must be supplied as a
-#' named list with \code{pred.locs} the locations where predictionas are to be made (referring to the first likelihood ONLY).
-#' If a spatio-tempral model is to be fitted \code{pred.temp} the temporal indecies for the predictions (the same length as \code{pred.locs}).
 #' @param verbose Logical if \code{TRUE} model fit is output to screen.
 #' 
 #' @export
 
-geo.joint.fit <- function(mesh = NULL,  locs = NULL, response = NULL, temp = NULL,covariates = NULL, family = "gaussian",
-                    control.time = list(model = 'ar1', param = list(theta = list(prior='pccor1', param = c(0, 0.9)))),
-                    control.inla = list(strategy='gaussian',int.strategy = 'eb'),
-                    control.compute = list(dic = TRUE, waic = TRUE,cpo = TRUE, config = TRUE),
-                    non.linear = NULL, prediction = NULL, verbose = FALSE){
-     if(is.null(temp)&is.null(covariates)){
+geo.joint.fit <- function(mesh = NULL,  locs = NULL, response = NULL, temp = NULL,covariates = NULL,
+                          family = c("gaussian","gaussian"),
+                          control.time = list(model = 'ar1', param = list(theta = list(prior='pccor1', param = c(0, 0.9)))),
+                          control.inla = list(strategy='gaussian',int.strategy = 'eb'),
+                          hyper = list(theta=list(prior='normal', param=c(0,10))),
+                          control.compute = list(dic = TRUE, waic = TRUE,cpo = TRUE, config = TRUE),
+                          non.linear = NULL,  verbose = FALSE){
+    if(is.null(temp)){
         fit <- geo.spatial.j.fit(mesh = mesh, locs = locs, response = response,covariates = NULL,
-                               family = family, control.inla = control.inla, control.compute = control.compute,
-                               non.linear = non.linear, prediction = prediction, verbose = verbose)
+                                 family = family, control.inla = control.inla, control.compute = control.compute,
+                                 hyper = hyper,
+                                 verbose = verbose)
     }
-    if(is.null(temp)&!is.null(covariates)){
-        fit <- geo.spatial.j.fit(mesh = mesh, locs = locs, response = response,covariates = covariates,
-                               family = family, control.inla = control.inla, control.compute = control.compute,
-                               non.linear = non.linear, prediction = prediction, verbose = verbose)
-    }
-    if(!is.null(temp)&is.null(covariates)){
+    if(!is.null(temp)&is.null(non.linear)){
         fit <- geo.spatial.j.temporal.fit(mesh = mesh, locs = locs, response = response, temp = temp,
-                                        family = family, covariates = NULL,
-                                        control.time = control.time, control.inla = control.inla,
-                                        control.compute = control.compute,
-                                        non.linear = non.linear, prediction = prediction, verbose = verbose)
+                                          family = family, covariates = NULL,
+                                          control.time = control.time, control.inla = control.inla,
+                                          hyper = hyper, control.compute = control.compute,
+                                          verbose = verbose)
     }
-    if(!is.null(temp)&!is.null(covariates)){
-        fit <- geo.spatial.j.temporal.fit(mesh = mesh, locs = locs, response = response,covariates = covariates,
-                                        temp = temp, family = family, control.time = control.time,
-                                        control.inla = control.inla, control.compute = control.compute,
-                                        non.linear = non.linear, prediction = prediction, verbose = verbose)
+    if(!is.null(temp)&!is.null(non.linear)){
+        fit <- geo.spatial.j.nl.temporal.fit(mesh = mesh, locs = locs, response = response,covariates = covariates,
+                                             temp = temp, family = family, control.time = control.time,
+                                             control.inla = control.inla, hyper = hyper, control.compute = control.compute,
+                                             non.linear = non.linear,  verbose = verbose)
     }
     return(fit)
 }
 
+
 #' spatial only fitting
 #' 
-geo.spatial.j.fit <- function(mesh, locs, response, covariates, family, control.time, control.inla,
-                              control.compute, non.linear, prediction, verbose){
+geo.spatial.j.fit <- function(mesh, locs, response, covariates, family,  control.inla,
+                              hyper, control.compute, verbose){
     spde <-inla.spde2.matern(mesh = mesh, alpha = 2)
     nv <- mesh$n
+    response.1 <- response[[1]]
+    response.2 <- response[[2]]
+    locs.1 <- locs[[1]]
+    locs.2 <- locs[[2]]
     Ast1 <- inla.spde.make.A(mesh = mesh, loc = locs.1)
     Ast2 <- inla.spde.make.A(mesh = mesh, loc = locs.2)
     field.1 <- field.2 <-  copy.field <-1:nv
     if(!is.null(covariates)){
-            m <- make.covs(covariates)
-            cov.effects <- m[[1]]
-            cov.form <- m[[2]]
-                                        #create data stacks
-            stk.pp <- inla.stack(data=list(y=cbind(response.1,NA)),
-                                 A=list( Ast1,1,1),
-                                 effects=list(field.1 = field.1, beta0 = rep(1,nrow(locs.1)),cov.effects = cov.effects))
-            x = "\"field.1\""
-            formula = paste("y", "~  0 + beta0 + alpha0 +", cov.form,
-                    " + f(field.1, model=spde)",
-                    "+ f(field.2, model=spde)",
-                    "+ f(copy.field, copy =", x, ",fixed=FALSE )")
+        m.1 <- make.covs(covariates[[1]])
+        m.2 <- make.covs(covariates[[2]])
+        cov.effects.1 <- m.1[[1]]
+        cov.form.1 <- m.1[[2]]
+        cov.effects.2 <- m.2[[1]]
+        cov.form.2 <- m.2[[2]]
+        stk.1 <- inla.stack(data=list(y=cbind(response.1,NA)),
+                             A=list( Ast1,1,1),
+                            effects=list(field.1 = field.1, beta0 = rep(1,nrow(locs.1)),cov.effects = cov.effects.1))
+        stk.2 <- inla.stack(data=list(y=cbind(NA,response.2)),
+                             A=list( Ast2,Ast2,1,1),
+                            effects=list(field.2 = field.2,copy.field = copy.field, alpha0 = rep(1,nrow(locs.2)),cov.effects = cov.effects.2))
+        stack <- inla.stack(stk.1,stk.2)
+        x = "\"field.1\""
+        formula = paste("y", "~  0 + beta0 + alpha0 +", cov.form.1, cov.form.2,
+                        " + f(field.1, model=spde)",
+                        "+ f(field.2, model=spde)",
+                    "+ f(copy.field, copy =", x, ",fixed=FALSE, hyper = hyper )")
     }else{
-                 stk.pp <- inla.stack(data=list(y=cbind(response.1,NA)),
-                                      A=list( Ast1,1),
-                                      effects=list(field.1 = field.1, beta0 = rep(1,nrow(locs.1))))
-                 formula <- y ~ 0 + beta0 + alpha0 + f(field.1, model=spde) +
-                      f(field.2, model=spde) +
-                     f(copy.field, copy = "field.1", fixed=FALSE, hyper = hyper )
+        stk.1 <- inla.stack(data=list(y=cbind(response.1,NA)),
+                             A=list( Ast1,1),
+                             effects=list(field.1 = field.1, beta0 = rep(1,nrow(locs.1))))
+        stk.2 <- inla.stack(data=list(y=cbind(NA,response.2)),
+                             A=list( Ast2,Ast2,1),
+                            effects=list(field.2 = field.2, copy.field = copy.field,alpha0 = rep(1,nrow(locs.2))))
+        stack <- inla.stack(stk.1,stk.2)
+        formula <- y ~ 0 + beta0 + alpha0 + f(field.1, model=spde) +
+            f(field.2, model=spde) +
+            f(copy.field, copy = "field.1", fixed=FALSE, hyper = hyper )
     }
-    stk.mark <- inla.stack(data=list(y=cbind(NA,response.2)),
-                               A=list(Ast2, Ast2,1),
-                           effects=list(field.2 = field.2, copy.field = copy.field, alpha0 = rep(1,nrow(locs.2))))
-    ## combine data stacks
-    stack <- inla.stack(stk.pp,stk.mark)
     result <- inla(as.formula(formula), family = family,
                    data = inla.stack.data(stack),
                    control.predictor=list(A=inla.stack.A(stack)),
@@ -108,46 +114,54 @@ geo.spatial.j.fit <- function(mesh, locs, response, covariates, family, control.
 
 #' spatio-temporal model fitting 
 geo.spatial.j.temporal.fit <-function(mesh, locs, response, covariates, temp, family, control.time, control.inla,
-                              control.compute, non.linear, prediction, verbose){
+                              hyper, control.compute,  verbose){
     spde <-inla.spde2.matern(mesh = mesh, alpha = 2)
     nv <- mesh$n
-    temp <- t.index # temporal dimension
+    temp <- temp # temporal dimension
     k <- (mesh.t <- inla.mesh.1d(temp))$n # number of groups
     ## create projection matrix for loacations
-        Ast.1 <- inla.spde.make.A(mesh = mesh, loc = locs.1, group = temp, n.group = k)
-    Ast.2 <- inla.spde.make.A(mesh = mesh, loc = locs.2, group = temp, n.group = k)
+    response.1 <- response[[1]]
+    response.2 <- response[[2]]
+    locs.1 <- locs[[1]]
+    locs.2 <- locs[[2]]
+    Ast1 <- inla.spde.make.A(mesh = mesh, loc = locs.1, group = temp, n.group = k)
+    Ast2 <- inla.spde.make.A(mesh = mesh, loc = locs.2, group = temp, n.group = k)
     field.1 <- inla.spde.make.index('field.1', n.spde = spde$n.spde, group = temp, n.group = k)
     field.2 <- inla.spde.make.index('field.2', n.spde = spde$n.spde, group = temp, n.group = k)
     copy.field <- inla.spde.make.index('copy.field', n.spde = spde$n.spde, group = temp, n.group = k)
-                                        # temporal model "ar1"
-    ctr.g <- list(model='ar1',param = prior.rho)
     if(!is.null(covariates)){
-            m <- lgcpSPDE:::make.covs(covariates)
-            cov.effetcs <- m[[1]]
-            cov.form <- m[[2]]
-                                        #create data stacks
-            stk.pp <- inla.stack(data=list(y=cbind(response.1,NA)),
-                                 A=list(Ast.1,1,1),
-                                 effects=list(field.1 = field.1, beta0 = rep(1,nrow(locs.1)), cov.effets = cov.effects))
-            x = "\"field.1\""
-            formula = paste("y", "~  0 + beta0 + alpha0 +", cov.form,
-                    " + f(field.1, model=spde, group = field.1.group, control.group=ctr.g)",
-                    "+ f(field.2, model=spde, group = field.2.group , control.group=ctr.g)",
-                    "+ f(copy.field, copy =", x, ",fixed=FALSE )")
+        m.1 <- make.covs(covariates[[1]])
+        m.2 <- make.covs(covariates[[2]])
+        cov.effects.1 <- m.1[[1]]
+        cov.form.1 <- m.1[[2]]
+        cov.effects.2 <- m.2[[1]]
+        cov.form.2 <- m.2[[2]]
+        stk.1 <- inla.stack(data=list(y=cbind(response.1,NA)),
+                            A=list( Ast1,1,1),
+                            effects=list(field.1 = field.1, beta0 = rep(1,nrow(locs.1)),cov.effects = cov.effects.1))
+        stk.2 <- inla.stack(data=list(y=cbind(NA,response.2)),
+                            A=list( Ast2,Ast2,1,1),
+                            effects=list(field.2 = field.2,copy.field = copy.field,
+                                         alpha0 = rep(1,nrow(locs.2)),cov.effects = cov.effects.2))
+        stack <- inla.stack(stk.1,stk.2)
+        x = "\"field.1\""
+        cov.form <- paste(cov.form.1,"+",cov.form.2)
+        formula = paste("y", "~  0 + beta0 + alpha0 +", cov.form,
+                        " + f(field.1, model=spde, group = field.1.group, control.group = control.time)",
+                        "+ f(field.2, model=spde, group = field.2.group , control.group = control.time)",
+                    "+ f(copy.field, copy =", x, ",fixed=FALSE, hyper = hyper )")
     }else{
-        stk.pp <- inla.stack(data=list(y=cbind(response.1,NA)),
-                             A=list(Ast.1,1),
-                             effects=list(field.1 = field.1, beta0 = rep(1,nrow(locs.1))))
-        formula <- y ~ 0 + beta0 + alpha0 + f(field.1, model=spde, group = field.1.group, control.group=ctr.g) +
-            f(field.2, model=spde, group = field.2.group , control.group=ctr.g) +
-                      f(copy.field, copy = "field.1", fixed=FALSE, hyper = hyper )
+        stk.1 <- inla.stack(data=list(y=cbind(response.1,NA)),
+                            A=list( Ast1,1),
+                            effects=list(field.1 = field.1, beta0 = rep(1,nrow(locs.1))))
+        stk.2 <- inla.stack(data=list(y=cbind(NA,response.2)),
+                            A=list( Ast2,Ast2,1),
+                            effects=list(field.2 = field.2, copy.field = copy.field,alpha0 = rep(1,nrow(locs.2))))
+        stack <- inla.stack(stk.1,stk.2)
+        formula <- y ~ 0 + beta0 + alpha0 + f(field.1, model=spde) +
+            f(field.2, model=spde) +
+            f(copy.field, copy = "field.1", fixed=FALSE, hyper = hyper )
     }
-    stk.mark <- inla.stack(data=list(y=cbind(NA,response.2)),
-                           A=list(Ast.2, Ast.2,1),
-                           effects=list(field.2 = field.2, copy.field = copy.field, alpha0 = rep(1,nrow(locs.2))))
-    ## combine data stacks
-    stack <- inla.stack(stk.pp,stk.mark)
-    
     result <- inla(as.formula(formula), family = family,
                    data = inla.stack.data(stack),
                    control.predictor=list(A=inla.stack.A(stack)),
@@ -156,5 +170,68 @@ geo.spatial.j.temporal.fit <-function(mesh, locs, response, covariates, temp, fa
                    verbose = verbose)
     return(result)
 }
-                                      
-    
+
+
+
+geo.spatial.j.nl.temporal.fit <-function(mesh, locs, response, covariates, temp, family, control.time, control.inla,
+                              hyper, non.linear, control.compute,  verbose){
+    spde <-inla.spde2.matern(mesh = mesh, alpha = 2)
+    nv <- mesh$n
+    temp <- temp # temporal dimension
+    k <- (mesh.t <- inla.mesh.1d(temp))$n # number of groups
+    ## create projection matrix for loacations
+    response.1 <- response[[1]]
+    response.2 <- response[[2]]
+    locs.1 <- locs[[1]]
+    locs.2 <- locs[[2]]
+    u <- non.linear[["random.effect"]]
+    u.mod <- non.linear[["model"]]
+    Ast1 <- inla.spde.make.A(mesh = mesh, loc = locs.1, group = temp, n.group = k)
+    Ast2 <- inla.spde.make.A(mesh = mesh, loc = locs.2, group = temp, n.group = k)
+    field.1 <- inla.spde.make.index('field.1', n.spde = spde$n.spde, group = temp, n.group = k)
+    field.2 <- inla.spde.make.index('field.2', n.spde = spde$n.spde, group = temp, n.group = k)
+    copy.field <- inla.spde.make.index('copy.field', n.spde = spde$n.spde, group = temp, n.group = k)
+    if(!is.null(covariates)){
+        m.1 <- make.covs(covariates[[1]])
+        m.2 <- make.covs(covariates[[2]])
+        cov.effects.1 <- m.1[[1]]
+        cov.form.1 <- m.1[[2]]
+        cov.effects.2 <- m.2[[1]]
+        cov.form.2 <- m.2[[2]]
+        stk.1 <- inla.stack(data=list(y=cbind(response.1,NA)),
+                            A=list( Ast1,1,1,1),
+                            effects=list(field.1 = field.1, beta0 = rep(1,nrow(locs.1)),cov.effects = cov.effects.1,
+                                         random = u))
+        stk.2 <- inla.stack(data=list(y=cbind(NA,response.2)),
+                            A=list( Ast2,Ast2,1,1),
+                            effects=list(field.2 = field.2,copy.field = copy.field,
+                                         alpha0 = rep(1,nrow(locs.2)),cov.effects = cov.effects.2))
+        stack <- inla.stack(stk.1,stk.2)
+        x = "\"field.1\""
+        cov.form <- paste(cov.form.1,"+",cov.form.2)
+        formula = paste("y", "~  0 + beta0 + alpha0 + f(random, model = u.mod) +", cov.form,
+                        " + f(field.1, model=spde, group = field.1.group, control.group = control.time)",
+                        "+ f(field.2, model=spde, group = field.2.group , control.group = control.time)",
+                    "+ f(copy.field, copy =", x, ",fixed=FALSE, hyper = hyper )")
+    }else{
+        stk.1 <- inla.stack(data=list(y=cbind(response.1,NA)),
+                            A=list( Ast1,1,1),
+                            effects=list(field.1 = field.1, beta0 = rep(1,nrow(locs.1)),
+                                         random = u))
+        stk.2 <- inla.stack(data=list(y=cbind(NA,response.2)),
+                            A=list( Ast2,Ast2,1),
+                            effects=list(field.2 = field.2, copy.field = copy.field,alpha0 = rep(1,nrow(locs.2))))
+        stack <- inla.stack(stk.1,stk.2)
+        formula <- y ~ 0 + beta0 + alpha0 + f(field.1, model=spde) +  f(random, model = u.mod) +
+            f(field.2, model=spde) +
+            f(copy.field, copy = "field.1", fixed=FALSE, hyper = hyper )
+    }
+    result <- inla(as.formula(formula), family = family,
+                   data = inla.stack.data(stack),
+                   control.predictor=list(A=inla.stack.A(stack)),
+                   control.inla = control.inla,
+                   control.compute = control.compute,
+                   verbose = verbose)
+    return(result)
+}
+                         
