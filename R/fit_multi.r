82eebb1f8e9ed1e052e0248f1f7bdef9d47cb94b
cmjt <cmjt@st-andrews.ac.uk>
2017-02-27 15:13:17 +1300

    bird paper

---
+++ R/fit_multi.r
@@ -0,0 +1,109 @@
+### function to fit model used in bird paper
+#' @param locs BTO GBFS site locations
+#' @param mesh Delauney triangulation of the UK
+#' @param temp years of GBFS
+#' @param binary.response list of length three. each refering to presence = 1, absence = 0 of the bird species
+#' sparrowhawk, collared dove, house sparrow respectively
+#' @param density.response list of length three. each refering to non-zero density of the bird species
+#' sparrowhawk, collared dove, house sparrow respectively.
+#' @param family a character vector of length two specifying the assumed likelihood of each species' response, by default
+#' is c("binomial","gamma").
+#' @param control.time (optional) supplied if the \code{temp} argumet is given to fit a spatio-temporal model. This argument
+#' controls the model and prior put on the hyperparameters of the model for the temporal component of the spatio-temporal
+#' model. By default this is \code{list(model = 'ar1', param = list(theta = list(prior='pccor1', param = c(0, 0.9))))}
+#' which is a pc.prior put on the rho coefficient of a AR(1) model with P(rho>0)=0.9.
+#' Refer to Simpson, martins, and rue for further details *****put in proper refs*****
+#' @param control.inla a list which controls the fitting procedures INLA uses see Rue et al. ***ref book***
+#' by default this is \code{list(strategy='gaussian',int.strategy = 'eb')} for quick and dirty fitting.
+#' @param hyper a list (of length 2) of lists of priors for each copy parameter. The first list has length 3
+#' specifying the priors on the intra species interaction parameters ( c(beta_1, beta_2, beta_3) i.e., sparrowhawk, collared dove, house sparrow resp.).
+#' the second element is of length 4 specifying the priors on the inter species interaction parameters
+#' in order thes refer to the parameters beta_z3, gamma_z3, beta_y3, and gamma_y3 (see model definition).
+#' By default each is a N(0,10) (i.e.,  list(theta=list(prior='normal', param=c(0,10))))
+#' @param control.compute a list of fit statistics the user wants INLA to return. By default this
+#' is \code{list(dic = TRUE, waic = TRUE,cpo = TRUE, config = TRUE)}.
+#' @param sig0 by default = 1, typical standard deviation to use pc priors for hyperparams of spde model
+#' @param rho0 by default = 0.3, typical range to use pc priors for hyperparams of spde model
+#' @param verbose Logical if \code{TRUE} model fit is output to screen.
+
+fit.multi <- function(locs = NULL, mesh = NULL, temp = NULL, binary.response = NULL, density.response = NULL,
+                      family = c("binomial","gamma"),
+                      hyper = list(intra = list(beta_1 = list(theta=list(prior='normal', param=c(0,10))),
+                                                beta_2 = list(theta=list(prior='normal', param=c(0,10))),
+                                                beta_3 = list(theta=list(prior='normal', param=c(0,10)))),
+                                   inter = list(beta_z3 = list(theta=list(prior='normal', param=c(0,10)))
+                                                gamma_z3 = list(theta=list(prior='normal', param=c(0,10)))
+                                                beta_y3 = list(theta=list(prior='normal', param=c(0,10)))
+                                                gamma_y3 = list(theta=list(prior='normal', param=c(0,10))))),
+                      control.time = list(model = 'ar1', param = list(theta = list(prior='pccor1', param = c(0, 0.9)))),
+                      control.inla = list(strategy='gaussian',int.strategy = 'eb'),
+                      control.compute = list(dic = TRUE, waic = TRUE,cpo = TRUE, config = TRUE),
+                      sig0 = 1, rho0 = 0.3, verbose = FALSE){
+    spde <- lgcpSPDE:::inla.spde2.matern.new(mesh, prior.pc.rho = c(rho0, 0.5), prior.pc.sig = c(sig0, 0.5))
+    k <- length(table(temp))
+    A <- inla.spde.make.A(mesh, loc = locs,group = temp)
+    zsp <- binary.response[[1]]
+    ysp <- density.response[[1]]
+    zco <- binary.response[[2]]
+    yco <- density.response[[2]]
+    zhs <- binary.response[[3]]
+    yhs <- density.response[[3]]
+    rf1 <- inla.spde.make.index('rf1',n.spde=spde$n.spde,n.group=k)
+    brf1 <- inla.spde.make.index('bfr1',n.spde=spde$n.spde,n.group=k)
+    rf2 <- inla.spde.make.index('rf2',n.spde=spde$n.spde,n.group=k)
+    brf2 <- inla.spde.make.index('brf2',n.spde=spde$n.spde,n.group=k)
+    g1rf1 <- inla.spde.make.index('g1rf1',n.spde=spde$n.spde,n.group=k)
+    g2rf2 <- inla.spde.make.index('g2rf2',n.spde=spde$n.spde,n.group=k)
+    rf3 <- inla.spde.make.index('rf3',n.spde=spde$n.spde,n.group=k)
+    g3rf1 <- inla.spde.make.index('g3rf1',n.spde=spde$n.spde,n.group=k)
+    g4rf2 <- inla.spde.make.index('g4rf2',n.spde=spde$n.spde,n.group=k)
+    brf3 <- inla.spde.make.index('brf3',n.spde=spde$n.spde,n.group=k)
+    b1 <- hyper$intra[[1]]
+    b2 <- hyper$intra[[2]]
+    b3 <- hyper$intra[[3]]
+    g1 <- hyper[[2]]$inter[[1]]
+    g2 <- hyper[[2]]$inter[[2]]
+    g3 <- hyper[[2]]$inter[[3]]
+    g4 <- hyper[[2]]$inter[[4]]
+    stk.zsp <- inla.stack(data=list(y=cbind(zsp, NA,NA,NA,NA,NA)),
+                          A=list(A), tag='sp.z',
+                          effects=list(rf1 = rf1 ))
+    stk.ysp<-inla.stack(data=list(y=cbind(NA,ysp,NA,NA,NA,NA)),tag="sp.y",
+                        A=list(A),
+                        effects=list(brf1 = brf1))
+    stk.zco <- inla.stack(data=list(y=cbind(NA,NA,zco, NA,NA,NA)),
+                          A=list(A), tag='co.z',
+                          effects=list(rf2 = rf2))
+    stk.yco<-inla.stack(data=list(y=cbind(NA,NA,NA,yco,NA,NA)),tag="co.y",
+                        A=list(A),
+                        effects=list(brf2 = brf2))
+    stk.zhs <- inla.stack(data=list(y=cbind(NA,NA,NA,NA,zhs, NA)),
+                          A=list(A,A,A,1), tag='hs.z',
+                          effects=list(g1rf1 = g1rf1,g2rf2 = g2rf2,rf3 = rf3,
+                                       alpha1=rep(1,length(zhs))))
+    stk.yhs<-inla.stack(data=list(y=cbind(NA,NA,NA,NA,NA,yhs)),tag="hs.y",
+                        A=list(A,A,A,1),
+                        effects=list(g3rf1 = g3rf1,g4rf2 = g4rf2,brf3 = brf3,
+                                     alpha2=rep(1,length(yhs))))
+    stack<-inla.stack(stk.zsp,stk.ysp,stk.zco,stk.yco,stk.zhs,stk.yhs)
+    formula<-y ~ 0 + alpha1 + alpha2 +
+        f(rf1, model = spde, control.group = control.time) +
+        f(brf1, copy = 'rf1' , fixed = FALSE,hyper=b1) +
+        f(rf2, model=spde, control.group = control.time) +
+        f(brf2, copy = 'rf2' , fixed = FALSE,hyper = b2) +
+        f(g1rf1, copy = 'rf1' , fixed = FALSE,hyper = g1) +
+        f(g2rf2, copy = 'rf2' , fixed = FALSE,hyper = g2) +
+        f(g3rf1, copy = 'rf1' , fixed = FALSE,hyper = g3) +
+        f(g4rf2, copy = 'rf2' , fixed = FALSE,hyper = g4) +
+        f(rf3, model = spde) +
+        f(brf3, copy = 'rf3' , fixed = FALSE,hyper = b3)
+    result <- inla(formula,
+                   family = family,
+                   data = inla.stack.data(stack),
+                   control.predictor=list(A=inla.stack.A(stack)),
+                   control.inla = control.inla,
+                   control.compute = control.compute,
+                   verbose = verbose)
+    
+    
+}
