29a2b80a4f31a1f147fff01863a8d24ed76024d1
cmjt <cmjt@st-andrews.ac.uk>
2018-07-23 17:37:03 +1200

    proj dimensions

--- R/plots.r
+++ R/plots.r
@@ -1,8 +1,5 @@
 #' Function that extracts the ''random fields'' of the model fitted.
 #'
-#' Plots the estimated random fields or parameter posterior densities from an object returned by
-#' \link{mark.pp.fit}().
-#'
 #' @param x A fitted model from \link{mark.pp.fit}().
 #' @param mesh the mesh used in the model fit.
 #' @param n.t numeric, the number of time points.
@@ -11,15 +8,17 @@
 #' random fields are plotted.
 #' @param spatial.polygon Optional, if a spatial polygon of the domain is supplied, only
 #' values of the random field within the domain will be returned
+#' @param dims vector of length two specifying spatial resolution of projection of fields onto the mesh
 #' @param ... additional graphical parameters
 #'  @importFrom fields image.plot
 #'
 #' @export
-find.fields <- function(x = NULL, mesh = NULL, n.t = NULL, sd = FALSE, plot = FALSE, spatial.polygon = NULL,...){
+find.fields <- function(x = NULL, mesh = NULL, n.t = NULL, sd = FALSE, plot = FALSE, spatial.polygon = NULL,
+                        dims = c(100,100),...){
     if(is.null(attributes(x)$mesh) & is.null(mesh)){
         stop("no mesh has been supplied")}
     if(!is.null(attributes(x)$mesh)){mesh <- attributes(x)$mesh}else{mesh <- mesh}
-    proj <- inla.mesh.projector(mesh)
+    proj <- inla.mesh.projector(mesh,dims = dims)
     if(!is.null(spatial.polygon)) inside <- inwin(proj,as.owin(spatial.polygon))
     spde <-inla.spde2.matern(mesh = mesh, alpha = 2)
     fields <- summary(x)$random.names[summary(x)$random.model=="SPDE2 model" | summary(x)$random.model=="Copy"]
@@ -29,11 +28,15 @@ find.fields <- function(x = NULL, mesh = NULL, n.t = NULL, sd = FALSE, plot = FA
         t <- n.t
         means <- list()
         for (i in idx[1]:idx[n]){
-            means [[i-idx[1]+1]] <- lapply(1:t, function(j) { r <- inla.mesh.project(proj, field = x$summary.random[[i]]$mean[1:spde$n.spde + (j-1)*spde$n.spde]);  if(!is.null(spatial.polygon)) r[!inside] <- NA; return(r)})
+            means [[i-idx[1]+1]] <- lapply(1:t, function(j) {
+                r <- inla.mesh.project(proj, field = x$summary.random[[i]]$mean[1:spde$n.spde + (j-1)*spde$n.spde])
+                if(!is.null(spatial.polygon)) r[!inside] <- NA; return(r)})
         }
         sds <- list()
         for (i in idx[1]:idx[n]){
-            sds [[i-idx[1]+1]] <- lapply(1:t, function(j) {r <- inla.mesh.project(proj, field = x$summary.random[[i]]$sd[1:spde$n.spde + (j-1)*spde$n.spde]);if(!is.null(spatial.polygon)) r[!inside] <- NA;  return(r)})
+            sds [[i-idx[1]+1]] <- lapply(1:t, function(j) {
+                r <- inla.mesh.project(proj, field = x$summary.random[[i]]$sd[1:spde$n.spde + (j-1)*spde$n.spde])
+                if(!is.null(spatial.polygon)) r[!inside] <- NA;  return(r)})
         }
         if(plot){plot.fields( x = x, mesh = mesh, n.t = n.t, sd = sd, spatial.polygon = spatial.polygon,...)}
     }else{
@@ -41,12 +44,12 @@ find.fields <- function(x = NULL, mesh = NULL, n.t = NULL, sd = FALSE, plot = FA
         for (i in idx[1]:idx[n]){
             means[[i-idx[1]+1]] <- inla.mesh.project(proj,x$summary.random[[i]]$mean)
             if(!is.null(spatial.polygon)) means[[i-idx[1]+1]][!inside] <- NA; 
-            }
+        }
         sds <- list()
         for (i in idx[1]:idx[n]){
             sds[[i-idx[1]+1]] <- inla.mesh.project(proj,x$summary.random[[i]]$sd)
             if(!is.null(spatial.polygon)) sds[[i-idx[1]+1]][!inside] <- NA; 
-            }
+        }
         if(plot){plot.fields( x = x, mesh = mesh, n.t = n.t, sd = sd, spatial.polygon = spatial.polygon,...)}
     }
     names(means) <- names(sds) <- fields
--- R/plotting.r
+++ R/plotting.r
@@ -1,6 +1,8 @@
-##plots fields eith spatio-temporal or spatial; function that calls a function that calls it!!
+#' Plots fields eith spatio-temporal or spatial
+#' @inheritParams find.fields
+#' @export
 plot.fields <- function(x = NULL, mesh = NULL, n.t = NULL, sd = FALSE, spatial.polygon = NULL,col = grey.colors(100,0.05,0.95),...){
-    proj <- inla.mesh.projector(mesh)
+    proj <- inla.mesh.projector(mesh,dims = dims)
     fields <- summary(x)$random.names[summary(x)$random.model=="SPDE2 model" | summary(x)$random.model=="Copy"]
     idx <- which(summary(x)$random.model=="SPDE2 model" | summary(x)$random.model=="Copy")
     n <- length(fields)
@@ -23,7 +25,7 @@ plot.fields <- function(x = NULL, mesh = NULL, n.t = NULL, sd = FALSE, spatial.p
 }
 
 
-## plot mesh
+#' plots a tidy mesh (internal function)
 plot.mesh <- function(x,...){
     plot(x,main="",asp=1,draw.segment = FALSE,...)
     if (!is.null(x$segm$bnd)) 
