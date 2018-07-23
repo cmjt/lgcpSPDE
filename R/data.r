5e96523c78a6d1785cdc638067149fb930498417
cmjt <cmjonestodd@gmail.com>
2018-07-24 08:03:01 +1200

    adding cleaned aggregated terrorism example data

--- R/data.r
+++ R/data.r
@@ -7,7 +7,7 @@
 #' \item{latitude}{latitude location}
 #' \item{longitude}{longitude location}
 #' \item{iyear}{year index 2010--2016}
-#' \item{x.coord}{x coordinate from location projected onto a spheree}
+#' \item{x.coord}{x coordinate from location projected onto a sphere}
 #' \item{y.coord}{y coordinate from location projected onto a sphere}
 #' \item{z.coord}{z coordinate from location projected onto a sphere}
 #' \item{fatal}{binary, 1 = fatalities occured, 0 = no fatalities}
@@ -15,7 +15,6 @@
 #' @docType data
 #' @usage data(terrorism)
 NULL
-
 #' Spatial polygon of countries worldwide
 #' @name world
 #' @format A Spatial Polygons Data Frame of contries worldwide
@@ -44,7 +43,7 @@ NULL
 #' @docType data
 #' @usage data(earthquakes)
 NULL
-#' A simulated dataset indicating breeding pair presence of Grus Grus in the wetlands on England.
+#' A simulated dataset indicating breeding pair presence of Grus Grus in the wetlands of England.
 #' @details Data simulated by Andrea Soriano Redondo (A.Soriano-Redondo@exeter.ac.uk) for the paper
 #' "Estimating species distribution in highly dynamic populations using point process models" submitted to Ecography.
 #' @name cranes
@@ -63,4 +62,37 @@ NULL
 #' }
 #' @docType data
 #' @usage data(cranes)
+NULL
+#' A simulated realisation of a log-Gaussian Cox process 2D
+#' @name lgcp2D
+#' @format A data frame containing 281 rowa and 2 variables:
+#' \describe{
+#' \item{x}{x-coordinate of the point pattern}
+#' \item{y}{y-coordinate of the point pattern}
+#' }
+#' @docType data
+#' @usage data(lgcp2D)
+NULL
+#' A cleaned dataset orginally taken from the global terrorism database (GTD) (http://www.start.umd.edu/gtd/)
+#' containing information of terrorism activity worldwide 2010--2016
+#' @name terrorism_data
+#' @format A data frame containing 8647 rows and 14 variables:
+#' \describe{
+#' \item{latitude}{latitude location}
+#' \item{longitude}{longitude location}
+#' \item{iyear}{year index 2010--2016}
+#' \item{total}{total number of events}
+#' \item{lethal}{number of lethal events}
+#' \item{nkill}{total number of fatalities}
+#' \item{alt}{altitude}
+#' \item{pop}{population}
+#' \item{tt}{time to nearest city (hours)}
+#' \item{lum}{luminosity}
+#' \item{country}{country code}
+#' \item{x.coord}{x coordinate of location projected onto a sphere}
+#' \item{y.coord}{y coordinate of location projected onto a sphere}
+#' \item{z.coord}{z coordinate of location projected onto a sphere}
+#' }
+#' @docType data
+#' @usage data(terrorism_data)
 NULL
