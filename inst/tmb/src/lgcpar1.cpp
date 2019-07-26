963a62cd357785950b1391edaaf87d499c360b71
cmjt <cmjt@st-andrews.ac.uk>
2019-07-26 18:46:58 +1200

    ar1 spde template, issues fitting but does compilr

--- inst/tmb/src/lgcpar1.cpp
+++ inst/tmb/src/lgcpar1.cpp
@@ -22,7 +22,7 @@ Type objective_function<Type>::operator() ()
   DATA_VECTOR(area);
   DATA_FACTOR(ID);
   DATA_STRUCT(spde,spde_t); // this as structure of spde object is defined in r-inla hence using that namespce
-  PARAMETER(beta0); // intercept term
+  PARAMETER(beta0); // fixed effect coefficients
   PARAMETER(log_kappa); // kappa of random field
   PARAMETER_ARRAY(x); //the random field/effect each matrix row is a time step
   PARAMETER(rho); // parameter of the AR(1) temporal process (only applicable for spatio-temporal model)
@@ -34,14 +34,16 @@ Type objective_function<Type>::operator() ()
   std::cout << resp(0).size() << "\n";
   st.setZero();
   nll = SEPARABLE(AR1(rho), GMRF(Q))(x); // x the random effect is a GMRF with precision Q
-  for(int j = 0; j < (tsteps - 1); j++){
-    vector<Type> respj = resp(j);
-    for(int i = 0; i <respj.size(); i++){
-      Type eta = beta0 + log(area(i)) + gmrf(i); 
-      Type lambda = exp(eta); // intensity 
-      nll -= dpois(respj(i),lambda,true); 
+  for(int i = 0; i< (tsteps - 1); i++){
+  vector<Type> gmrf = (vector<Type> (x.col(i)));
+  vector<Type> respi = resp(i);
+  for(int j = 0; j <respi.size(); j++){
+      Type mu;
+      mu = beta0 + area(j) + gmrf(j);
+      nll -= dpois(respi(j),mu,true);
+      
     }
- } 
+  }
   ADREPORT(kappa);
   Type sigma2 = 1/(4*M_PI*pow(kappa,2));   // sigma2 = 1/(4*pi*k^2) with tau =1
   ADREPORT(sigma2);
